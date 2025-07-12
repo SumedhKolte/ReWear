e2e21661575f83f7dd3315bf78c87d113d202cd0
sumedhkolte <sumedhkolte19@gmail.com>
2025-07-12 13:20:00 +0530

    Done with Auth added comp as well

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -1,22 +1,22 @@
 // src/routes/routes.jsx
-// import { Routes, Route } from 'react-router-dom';
-// import UserLandingPage from '../Pages/UserLandingPage';
-// import NotFound from '../Pages/Error Pages/NotFound'; // optional 404 fallback
-// import Signup from '../Pages/Auth/signup'; // Import the signup page
-// import ItemListingPage from '../Pages/itemlisting';
-import UserDashboard from "../Pages/userdashboard"
+import { Routes, Route } from 'react-router-dom';
+import UserLandingPage from '../Pages/UserLandingPage';
+import NotFound from '../Pages/Error Pages/NotFound'; // optional 404 fallback
+import Signup from '../Pages/Auth/signup'; // Import the signup page
+import ItemListingPage from '../Pages/itemlisting';
+// import UserDashboard from "../Pages/userdashboard"
 // import ItemListingPage from "../Pages/itemlisting";
 
 export default function AppRoutes() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<UserLandingPage />} />
-    //   <Route path="*" element={<NotFound />} />      
-    //   <Route path="/" element={<Signup />} />   
-    //   <Route path="/" element={<ItemListingPage />} />   
+    <Routes>
+      <Route path="/" element={<UserLandingPage />} />
+      <Route path="*" element={<NotFound />} />      
+      <Route path="/signup" element={<Signup />} />   
+      <Route path="/ItemListing" element={<ItemListingPage />} />   
 
-    // </Routes>
-    <UserDashboard />
+    </Routes>
+   
   );
 }
 
